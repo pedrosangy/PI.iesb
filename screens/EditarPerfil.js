5c5b3c60dcc3341a749b1d1f02b4694714784692
Matheus-AR <matheusaraujorodrigues@outlook.com>
2023-09-29 10:31:07 -0300

    close #14

---
+++ screens/EditarPerfil.js
@@ -0,0 +1,72 @@
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  ImageBackground,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+const EditarPerfil = () => {
+  return (
+    <ImageBackground
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
+      source={require("../assets/telaBackground2.png")}
+    >
+      <Image
+        style={{ position: "relative", top: -220, left: -135 }}
+        source={require("../assets/btnVoltar.png")}
+      />
+      <View
+        style={{
+          justifyContent: "space-around",
+          alignItems: "center",
+          backgroundColor: "white",
+          width: 270,
+          height: 270,
+          padding: 12,
+          borderRadius: 20,
+        }}
+      >
+        <Text
+          style={{
+            padding: 10,
+            borderRadius: 27,
+            backgroundColor: "#D9D9D9",
+            textAlign: "center",
+          }}
+        >
+          Editar Perfil
+        </Text>
+        <Text style={{ alignSelf: "stretch" }}>Usuário</Text>
+        <TextInput
+          style={{ backgroundColor: "#D9D9D9", alignSelf: "stretch" }}
+        />
+        <Text style={{ alignSelf: "stretch" }}>Descrição</Text>
+        <TextInput
+          style={{ backgroundColor: "#D9D9D9", alignSelf: "stretch" }}
+        />
+
+        <Text
+          style={{
+            backgroundColor: "#D9D9D9",
+            marginTop: 10,
+            borderRadius: 20,
+            padding: 5,
+            paddingHorizontal: 30,
+          }}
+        >
+          Confirmar
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default EditarPerfil;
